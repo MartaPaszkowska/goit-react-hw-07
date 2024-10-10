9a090013f125a9906c0f9792200eec27ff0d3681
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-10 15:52:03 +0200

    1

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -24,3 +24,12 @@ export const deleteContact = createAsyncThunk(
 	}
 );
 // redux/operations.js
+axios.interceptors.request.use((request) => {
+	console.log("Starting Request", request);
+	return request;
+});
+
+axios.interceptors.response.use((response) => {
+	console.log("Response:", response);
+	return response;
+});
