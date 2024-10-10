0a628c4f00f96f60dd1479da6927d849a2078337
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-10 15:55:22 +0200

    done

--- checkHeaders.js
+++
@@ -1,9 +0,0 @@
-axios.interceptors.request.use((request) => {
-	console.log("Starting Request", request);
-	return request;
-});
-
-axios.interceptors.response.use((response) => {
-	console.log("Response:", response);
-	return response;
-});
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -23,13 +23,3 @@ export const deleteContact = createAsyncThunk(
 		return id;
 	}
 );
-// redux/operations.js
-axios.interceptors.request.use((request) => {
-	console.log("Starting Request", request);
-	return request;
-});
-
-axios.interceptors.response.use((response) => {
-	console.log("Response:", response);
-	return response;
-});
