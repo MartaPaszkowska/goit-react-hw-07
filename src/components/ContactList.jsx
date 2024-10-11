85cff56a4f7afcb344600bcb8bd288253d8222d3
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-10-11 14:36:05 +0200

    1

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteContact } from "../redux/operations";
+import { deleteContact, fetchContacts } from "../redux/operations";
 import {
 	selectContacts,
 	selectFilter,
